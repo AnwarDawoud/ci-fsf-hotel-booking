32a447dadd01fd0948a6e8e86eb8eeb174d04d30
Anwar <enganwar100@hotmail.com>
2024-01-23 02:00:39 +0000

    Update requirements.txt for storages compatibility

--- hotels_booking/settings.py
+++ hotels_booking/settings.py
@@ -8,21 +8,21 @@ from django.db import models
 
 
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) 
+BASE_DIR = Path(__file__).resolve().parent.parent
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 
 ON_HEROKU = config('ON_HEROKU', default=False, cast=bool) 
 
 
-SECRET_KEY = '9q=3tig&^s7zoq@16ir2hz-q$+af^9tqy7=v^_b&i!uf0q8$%i' # Set DEBUG based on the environment
-
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 DEBUG = not ON_HEROKU # True if local, False if on Heroku
 
 
 ALLOWED_HOSTS = [
-    'mysterious-tundra-89304-deptes-8a08ec3a2b87.herokuapp.com',
-    '.localhost',
+    'mysterious-tundra-89304-deptes.herokuapp.com',
+    'localhost',
     '127.0.0.1'
 ] 
 
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'hotels_booking.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -71,17 +71,26 @@ TEMPLATES = [
 if ON_HEROKU:
     DATABASE_URL = os.environ.get('DATABASE_URL')
 else:
-    DATABASE_URL = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
+    DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
-DATABASES = {'default': dj_database_url.config(default=DATABASE_URL)}
+# DATABASES = {'default': dj_database_url.config(default=DATABASE_URL)}
 
-
+DATABASES = {
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+}
 
 MEDIA_URL = '/media/'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
  
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
+STATICFILES_DIRS = [os.path.jion(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 LOGIN_URL = 'login'
 
