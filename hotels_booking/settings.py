1a2e1757433741e19447116a5fbf076e0f85a1e9
Anwar <enganwar100@hotmail.com>
2024-01-23 02:17:27 +0000

    Update requirements.txt for storages compatibility

--- hotels_booking/settings.py
+++ hotels_booking/settings.py
@@ -89,7 +89,7 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
  
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
-STATICFILES_DIRS = [os.path.jion(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 LOGIN_URL = 'login'
