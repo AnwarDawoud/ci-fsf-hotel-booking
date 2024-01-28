26afa218544d346972b2a34c7c86e8cc29c52973
Anwar <enganwar100@hotmail.com>
2024-01-28 01:16:33 +0000

    Delete hotel migrations

--- hotels_booking/settings.py
+++ hotels_booking/settings.py
@@ -88,7 +88,7 @@ TEMPLATES = [
 # # Database configuration
 if ON_HEROKU:
     DATABASE_URL = os.environ.get('DATABASE_URL')
-    DATABASES = {'default': dj_database_url.config(default=DATABASE_URL, conn_max_age=600)}
+    DATABASES = {'default': dj_database_url.config(default=DATABASE_URL)}
 else:
     DATABASES = {
         'default': {
